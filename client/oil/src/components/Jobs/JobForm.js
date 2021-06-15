a9172b2127773c84efd9cf1dd8a01d0947acb58f
Alex Martin <alex@subtleco.co>
2021-06-14 21:36:53 -0500

    required entries on form not working, create call to API not working

--- client/oil/src/components/Jobs/JobForm.js
+++ client/oil/src/components/Jobs/JobForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { userIdStorageKey } from '../auth/authSettings'
 import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper'
 import MenuItem from '@material-ui/core/MenuItem'
 import Button from '@material-ui/core/Button'
 import { endsWith } from 'lodash'
+import { JobsContext } from './JobsProvider'
 
 const useStyles = makeStyles(theme => ({
     outerPaper: {
@@ -32,6 +33,7 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export const JobForm = props => {
+    const { createJob } = useContext(JobsContext)
     const classes = useStyles(props.theme);
     const userId = parseInt(sessionStorage.getItem(userIdStorageKey))
     const [formJob, setFormJob] = useState({
@@ -52,10 +54,14 @@ export const JobForm = props => {
         setFormJob(newFormJob)
     }
 
+    const handleSubmit = e => {
+        createJob(formJob)
+    }
+
     return (
         <div className="jobForm">
             <Paper className={classes.outerPaper} elevation={3}>
-                <FormControl className={classes.form} noValidate autoComplete="off">
+                <form onSubmit={handleSubmit} className={classes.form} noValidate autoComplete="off">
                     <h2>New Job</h2>
                     <TextField onChange={handleFormChange}
                         value={formJob.title}
@@ -105,8 +111,13 @@ export const JobForm = props => {
                         <MenuItem value={2}>Job</MenuItem>
                         <MenuItem value={3}>Types</MenuItem>
                     </TextField>
-                    <Button variant="contained" className={classes.submitButton}>Save</Button>
-                </FormControl>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        className={classes.submitButton}>
+                            Save
+                    </Button>
+                </form>
             </Paper>
         </div>
     )
