90491583eee19cb6c2ce1e0ec39bb86d410bb25e
Alex Martin <alex@subtleco.co>
2021-06-16 11:15:12 -0500

    cosmetic tweaks to border radius, margins, added comments to Today

--- client/oil/src/components/Jobs/Today.js
+++ client/oil/src/components/Jobs/Today.js
@@ -1,4 +1,13 @@
-import React, { useContext, useEffect } from 'react'
+// This module renders the main view of Oil
+
+// This view will display a list of jobs where the time since "last completed" has overlapped
+// "frequency" (days_lapsed >= 0), meaning the job needs to be completed again.
+
+// if days_lapsed is 0 - 2, list item will be green
+// if days_lapsed is 3 - 6, list item will be yellow
+// if days_lapsed is 7+, list item will be red
+
+import React, { useContext, useEffect, useState } from 'react'
 import { JobsContext } from './JobsProvider'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
@@ -20,7 +29,14 @@ const useStyles = makeStyles(theme => ({
             height: "fit-content",
         },
         marginBottom: "7vh",
-        marginTop: "12vh"
+        marginTop: "12vh",
+        borderRadius: "5px",
+    },
+    todayList: {
+        padding: 0,
+    },
+    todayPaper: {
+        borderRadius: "5px"
     },
     fabs: {
         margin: theme.spacing(1),
@@ -32,7 +48,7 @@ const useStyles = makeStyles(theme => ({
         paddingTop: "2vh"
     },
     lowPriority: {
-        background: theme.palette.success.light
+        background: theme.palette.success.light,
     },
     medPriority: {
         background: theme.palette.secondary.light
@@ -40,11 +56,20 @@ const useStyles = makeStyles(theme => ({
     highPriority: {
         background: theme.palette.error.light
     },
+    topItem: {
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    bottomItem: {
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+    },
 }))
 
 export const Today = props => {
     const history = useHistory()
     const { userJobs, getAllUserJobs, completeJob } = useContext(JobsContext)
+    const [dueJobs, setDueJobs] = useState([])
     const classes = useStyles()
 
     // Pretty day
@@ -57,39 +82,44 @@ export const Today = props => {
         getAllUserJobs()
     }, [])
 
+    useEffect(() => {
+        setDueJobs(userJobs.filter(j => j.days_lapsed >= 0))
+    }, [userJobs])
+
     const handleDone = e => {
-        const jobId = e.target.id.split("--")[1]
-        console.log(jobId)
+        const jobId = e.currentTarget.id.split("--")[1]
         completeJob(jobId)
+            .then(getAllUserJobs)
     }
 
     return (
         <>
             <div className={classes.root}>
                 <h2 className={classes.pageHeader}>Wow, it's already {niceDay}?</h2>
-                <Paper elevation={3}>
-                    <List>
+                { !!dueJobs.length && <Paper className={classes.todayPaper} elevation={3}>
+                    <List className={classes.todayList}>
                         {
-                            userJobs.map(j => {
-                                if (j.days_lapsed >= 0) {
-                                    let priority = classes.lowPriority
-                                    if (j.days_lapsed > 2) priority = classes.medPriority
-                                    if (j.days_lapsed > 6) priority = classes.highPriority
-                                    return (
-                                        <ListItem className={priority} key={j.id}>
-                                            <ListItemText primary={j.title} />
-                                            <ListItemIcon id={"list-item-icon-job--" + j.id}>
-                                                <Fab className={classes.fabs} onClick={handleDone} id={"job--" + j.id} aria-label="edit">
-                                                    <DoneIcon id={"icon-job--" + j.id}/>
-                                                </Fab>
-                                            </ListItemIcon>
-                                        </ListItem>
-                                    )
-                                }
+                            dueJobs.map((j,i) => {
+                                let priority = classes.lowPriority
+                                if (j.days_lapsed > 2) priority = classes.medPriority
+                                if (j.days_lapsed > 6) priority = classes.highPriority
+                                if (i == 0) priority = `${priority} ${classes.topItem}`
+                                if (i == dueJobs.length - 1) priority = `${priority} ${classes.bottomItem}`
+
+                                return (
+                                    <ListItem className={priority} key={j.id}>
+                                        <ListItemText primary={j.title} />
+                                        <ListItemIcon onClick={handleDone} id={"job--" + j.id}>
+                                            <Fab className={classes.fabs} aria-label="edit">
+                                                <DoneIcon />
+                                            </Fab>
+                                        </ListItemIcon>
+                                    </ListItem>
+                                )
                             })
                         }
                     </List>
-                </Paper>
+                </Paper>}
             </div>
         </>
     )
