f70508553a7698f4c25125772dafd8b9560a6861
Alex Martin <alex@subtleco.co>
2021-06-21 17:11:08 -0500

    A user can now edit their own profile

--- server/oil/oilapi/views/user.py
+++ server/oil/oilapi/views/user.py
@@ -7,13 +7,14 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 class UserSerializer(serializers.ModelSerializer):
     """JSON serializer for Users"""
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username']
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 
 class UserView(ViewSet):
@@ -34,6 +35,25 @@ class UserView(ViewSet):
                 return Response(serializer.data)
             except User.DoesNotExist as ex:
                 return Response(ex.args[0], status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, pk=None):
+        user = request.auth.user
+        if user.id != int(pk):
+            return Response("You cannot edit a user account that is not your own", status=status.HTTP_401_UNAUTHORIZED)
+
+        req = request.data
+        try:
+            edit_user = User.objects.get(pk=user.id)
+            edit_user.set_password(req['password'])
+            edit_user.username = req['username']
+            edit_user.email = req['email']
+            edit_user.first_name = req['first_name']
+            edit_user.last_name = req['last_name']
+
+            edit_user.save()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except User.DoesNotExist:
+            return Response("You cannot edit a user account that is not your own", status=status.HTTP_401_UNAUTHORIZED)
 
     # Get current user
     @action(methods=['get'], detail=False)
