d0ce2389432bf7ac82191b45b7248766c0aa0111
Alex Martin <alex@subtleco.co>
2021-06-22 09:57:22 -0500

    Moved header and footer to Application Views to share PeopleContext

--- client/oil/src/components/Oil.js
+++ client/oil/src/components/Oil.js
@@ -4,21 +4,15 @@ import { ApplicationViews } from './ApplicationViews'
 import { userTokenStorageKey } from './auth/authSettings'
 import { Login } from './auth/Login'
 import { Register } from './auth/Register'
-import { Footer } from './Footer/Footer'
-import { Header } from './Header/Header'
-import { PeopleProvider } from './People/PeopleProvider'
 
 export const Oil = ({ theme }) => {
     return (
         <>
-            <PeopleProvider>
                 <Route render={() => {
                     if (sessionStorage.getItem(userTokenStorageKey)) {
                         return (
                             <>
-                                <Header />
                                 <ApplicationViews />
-                                <Footer theme={theme} />
                             </>
                         )
                     } else {
@@ -33,7 +27,6 @@ export const Oil = ({ theme }) => {
                 <Route path="/register">
                     <Register />
                 </Route>
-            </PeopleProvider>
         </>
     )
 }
